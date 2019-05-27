dc7c911e6f2536e4a2873fbace236d65596d8c72
Marta Blaszczak <marta.blaszczak1@gmail.com>
2019-05-27 22:48:04 +0100

    Add style.scss

---
+++ js/script.js
@@ -0,0 +1,29 @@
+
+var elem = document.querySelector('.main-carousel');
+
+var flkty = new Flickity( elem, {
+  cellAlign: 'left',
+  contain: true,
+  pageDots: false,
+  fullscreen: true,
+  hash: true,
+  on: {
+    ready: function() {
+      console.log('Flickity ready');
+    }
+  }
+});
+
+
+var restartButton = document.querySelector('.restartButton');
+
+restartButton.addEventListener( 'click', function() {
+  flkty.select( 0 );;
+});
+
+var progressBar = document.querySelector('.progress-bar')
+
+flkty.on( 'scroll', function( progress ) {
+  progress = Math.max( 0, Math.min( 1, progress ) );
+  progressBar.style.width = progress * 100 + '%';
+});
